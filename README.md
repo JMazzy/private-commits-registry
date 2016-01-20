076a9e91cdf4ee61ea24b1ea4dd56d8837fc4235
Joshua Masland-Sarani <j.masland@gmail.com>
2016-01-20 01:00:07 +0000

    private commit

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 I just did a private commit!
 I just did a private commit!
 I just did a private commit! The time is: 2016-01-19 20:04:56 -0500
+I just did a private commit! The time is: 2016-01-20T01:00:07Z
--- github_script.rb
+++ github_script.rb
@@ -1,17 +1,45 @@
 require 'time'
+require 'github_api'
+require 'figaro'
+require 'pp'
 
-def committer(time)
-  time.xmlschema.to_s
+class GithubPrivateCommitter
+  USER_NAME = 'jmazzy'
 
-  # `cd ~/Copy/Viking/web_scraping/private_commits_registry`
+  def initialize
+    Figaro.application = Figaro::Application.new( {
+      environment: "development",
+      path:"./config/application.yml"} )
+    Figaro.load
 
-  readme = File.open("README.md", "a")
-  readme.puts("I just did a private commit! The time is: #{time}")
-  readme.close
+    @github = Github.new(oauth_token: Figaro.env.github_key)
+  end
 
-  `git add -A`
-  `git commit -m "private commit" --date #{time.xmlschema.to_s}`
-  `git push origin master`
+  def all_commits(commits)
+    commits.each do |commit|
+      commit["commit"]["message"]
+    end
+  end
+
+  def committer(time)
+    readme = File.open("README.md", "a")
+    readme.puts("I just did a private commit! The time is: #{time}")
+    readme.close
+
+    `git add -A`
+    `git commit -m "private commit" --date #{time}`
+    `git push origin master`
+  end
+
+  def get_commits(repo_name)
+    sleep(0.1)
+    @github.repos.commits.list(USER_NAME, repo_name, '...')
+  end
 end
 
-committer(Time.now)
+gpc = GithubPrivateCommitter.new
+
+gpc.get_commits('assignment_ruby_api_calls').each do |commit|
+  gpc.committer(commit["commit"]["committer"]["date"])
+end
+# gpc.committer(Time.now)
